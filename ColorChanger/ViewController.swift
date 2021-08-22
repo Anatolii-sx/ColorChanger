1c319badf8ee70b464f8cd987b383d5b4ed223cf
Anatolii-sx <s8.8@bk.ru>
2021-08-22 14:35:56 +0300

    Optimized code

--- ColorChanger/ViewController.swift
+++ ColorChanger/ViewController.swift
@@ -19,53 +19,52 @@ class ViewController: UIViewController {
     @IBOutlet var greenColorSlider: UISlider!
     @IBOutlet var blueColorSlider: UISlider!
     
-    private var red: Float = 0
-    private var green: Float = 0
-    private var blue: Float = 0
-    
-    private func makeColorOfView(red: Float, green: Float, blue: Float, label: String) {
-
-        colorOfView.backgroundColor = UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1
-        )
-        
-        switch label {
-        case "forRed":
-            self.red = red
-            redColorValueLabel.text = String(format: "%.2f", red)
-        case "forGreen":
-            self.green = green
-            greenColorValueLabel.text = String(format: "%.2f", green)
-        case "forBlue":
-            self.blue = blue
-            blueColorValueLabel.text = String(format: "%.2f", blue)
-        default:
-            self.red = red
-            self.green = green
-            self.blue = blue
-            redColorValueLabel.text = String(format: "%.2f", red)
-            greenColorValueLabel.text = String(format: "%.2f", green)
-            blueColorValueLabel.text = String(format: "%.2f", blue)
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         colorOfView.layer.cornerRadius = 10
-
-        makeColorOfView(red: 0.40, green: 1, blue: 0.6, label: "Start")
+        
+        makeColorOfView()
+        changeLabelValue(number: 0)
     }
     
     @IBAction func redSliderMoved() {
-        makeColorOfView(red: redColorSlider.value, green: green, blue: blue, label: "forRed")
+        makeColorOfView()
+        changeLabelValue(number: 1)
     }
     
     @IBAction func greenSliderMoved() {
-        makeColorOfView(red: red, green: greenColorSlider.value, blue: blue, label: "forGreen")
+        makeColorOfView()
+        changeLabelValue(number: 2)
     }
     
     @IBAction func blueSliderMoved() {
-        makeColorOfView(red: red, green: green, blue: blueColorSlider.value, label: "forBlue")
+        makeColorOfView()
+        changeLabelValue(number: 3)
+    }
+    
+    private func makeColorOfView() {
+        colorOfView.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func changeLabelValue(number: Int) {
+        switch number {
+        case 1:
+            redColorValueLabel.text = String(format: "%.2f", redColorSlider.value)
+        case 2:
+            greenColorValueLabel.text = String(format: "%.2f", greenColorSlider.value)
+        case 3:
+            blueColorValueLabel.text = String(format: "%.2f", blueColorSlider.value)
+        default:
+            redColorValueLabel.text = String(format: "%.2f", redColorSlider.value)
+            greenColorValueLabel.text = String(format: "%.2f", greenColorSlider.value)
+            blueColorValueLabel.text = String(format: "%.2f", blueColorSlider.value)
+        }
     }
 }
 
